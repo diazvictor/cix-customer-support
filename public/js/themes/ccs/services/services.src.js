26306c0fded8a33b2ec483cc80f534079b0f970a
diazvictor <victor.vector008@gmail.com>
2022-06-10 15:09:41 -0400

    Modulo terminado (Responsive y Desktop)

--- public/js/themes/ccs/services/services.src.js
+++ public/js/themes/ccs/services/services.src.js
@@ -1,8 +1,7 @@
-// import      '../common/ccs.js';
-import		"leaflet";
+import	    	       "leaflet";
+import 		Chart from "chart.js";
 import      '../common/navbar.js';
 import      '../common/sidebar.js';
-import      '../common/dropdown.js';
 
 let title = document.querySelector(".navbar #title");
 
@@ -21,6 +20,9 @@ if (window.location.pathname == "/services/") {
 } else if (window.location.pathname == "/services/new/") {
 	sidenav = document.querySelector(".sidenav a[data-item=services-new]");
 	title.innerText = "Nueva Solicitud";
+} else if (window.location.pathname == "/services/view/") {
+	sidenav = document.querySelector(".sidenav a[data-item=services]");
+	title.innerText = "Mis Servicios";
 }
 
 sidenav.classList.add("is-active");
@@ -39,7 +41,7 @@ let mbUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 let grayscale = L.tileLayer(mbUrl, {id: 'mapbox/light-v9', tileSize: 512, zoomOffset: -1, attribution: mbAttr});
 let streets = L.tileLayer(mbUrl, {id: 'mapbox/streets-v11', tileSize: 512, zoomOffset: -1, attribution: mbAttr});
 
-let map = L.map('mapid', {
+var map = L.map('mapid', {
     center: [9.742531,-63.149517],
     zoom: 10,
     layers: [grayscale, cities]
@@ -53,3 +55,76 @@ let baseLayers = {
 let overlays = {
     'Cities': cities
 };
+// Chart
+const labels = [
+	'Ene', 'Feb', 'Mzo',
+	'May', 'Jun', 'Jul',
+	'Ago', 'Sep', 'Oct',
+	'Nov', 'Dic'
+];
+
+const data = {
+	labels: labels,
+	datasets: [
+		{
+			label: 'Descarga Mbps',
+			// data: [0,1,2,3,4,5,6,7,8,9,10,11,12],
+			data: [4, 5, 10, 12, 25, 12, 23, 18, 25, 10, 35],
+			backgroundColor: [
+				'rgba(230, 60, 31, 0.4)',
+				'rgba(230, 60, 31, 0.4)',
+				'rgba(230, 60, 31, 0.4)',
+				'rgba(230, 60, 31, 0.4)',
+				'rgba(230, 60, 31, 0.4)',
+				'rgba(230, 60, 31, 0.4)',
+				'rgba(230, 60, 31, 0.4)',
+				'rgba(230, 60, 31, 0.4)',
+				'rgba(230, 60, 31, 0.4)',
+				'rgba(230, 60, 31, 0.4)',
+				'rgba(230, 60, 31, 0.4)',
+			],
+			borderColor: []
+		},
+		{
+			label: 'Subida Mbps',
+			data: [5, 3, 7, 9, 0.6, 2, 0.3, 15, 0.8, 1, 9],
+			backgroundColor: [
+				'rgba(230, 60, 31, 1)',
+				'rgba(230, 60, 31, 1)',
+				'rgba(230, 60, 31, 1)',
+				'rgba(230, 60, 31, 1)',
+				'rgba(230, 60, 31, 1)',
+				'rgba(230, 60, 31, 1)',
+				'rgba(230, 60, 31, 1)',
+				'rgba(230, 60, 31, 1)',
+				'rgba(230, 60, 31, 1)',
+				'rgba(230, 60, 31, 1)',
+				'rgba(230, 60, 31, 1)',
+				'rgba(230, 60, 31, 1)',
+			],
+			borderColor: []
+		}
+	]
+};
+
+const config = {
+	type: 'bar',
+	data: data,
+	options: {
+		scales: {
+			y: {
+				beginAtZero: true
+			}
+		}
+	},
+};
+
+let myChart = new Chart(
+	document.getElementById('myChart'),
+	config
+);
+
+let myChartTwo = new Chart(
+	document.getElementById('myChart2'),
+	config
+);
