aa41a5bbe260f9578038d56f81025b6e5cd51b28
diazvictor <victor.vector008@gmail.com>
2022-06-10 15:12:32 -0400

    Todos los script del modulo en un solo archivo

--- public/js/themes/ccs/reports/reports.src.js
+++ public/js/themes/ccs/reports/reports.src.js
@@ -11,10 +11,10 @@ sidenavCollapse.classList.add("is-active");
 
 let sidenav;
 
-if (window.location.pathname == "/reports") {
+if (window.location.pathname == "/reports/") {
 	sidenav = document.querySelector(".sidenav a[data-item=reports]");
 	title.innerText = "Mis Reportes";
-} else if (window.location.pathname == "/new") {
+} else if (window.location.pathname == "/reports/new/") {
 	sidenav = document.querySelector(".sidenav a[data-item=reports-new]");
 	title.innerText = "Nuevo Reporte";
 } else {
@@ -23,3 +23,25 @@ if (window.location.pathname == "/reports") {
 }
 
 sidenav.classList.add("is-active");
+
+const toggleContact = document.getElementById("toggle-contact");
+const toggleItems = document.querySelectorAll(".toggle-contact");
+
+toggleContact.addEventListener('change', (e) => {
+	toggleItems.forEach((item) => {
+		if (e.target.checked) {
+			item.style.setProperty('display', 'flex', 'important');
+		} else {
+			item.style.setProperty('display', 'none', 'important');
+		}
+	});
+})
+
+const filebutton = document.querySelector(".file");
+let fileinput = filebutton.querySelector("label input[type=file]");
+let filename = document.querySelector(".file input[type=text]");
+	
+fileinput.addEventListener("change", () => {
+	filename.value = fileinput.files[0].name;
+});
+
